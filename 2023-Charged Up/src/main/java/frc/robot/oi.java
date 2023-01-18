bf6b90e6c5591c87765be8ab2413c7d3f1b6f765
JaxonAn23 <59897973+JaxonAn23@users.noreply.github.com>
2023-01-17 19:10:45 -0500

    This is cool

--- 2023-Charged Up/src/main/java/frc/robot/oi.java
+++ 2023-Charged Up/src/main/java/frc/robot/oi.java
@@ -1,17 +1,26 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.XboxController;
 
-public class OI extends SubsystemBase {
-  /** Creates a new OI. */
-  public OI() {}
+import static frc.robot.Constants.*;
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+public class OI {
+
+    private static XboxController driveController;
+    private static XboxController cmdController;
+
+    //public OI () 
+    static {
+      driveController = new XboxController(1);
+      cmdController = new XboxController(2);
+    }
+
+    public static XboxController getDriver() {
+      return driveController;
+    }
+
+    public static XboxController getCMDController() {
+      return cmdController;
+    }
+
 }
