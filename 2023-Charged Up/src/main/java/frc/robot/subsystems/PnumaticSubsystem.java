f897e8291aa0aac64456c7252e672d0c69c58af5
zirppo <zirppo@protonmail.com>
2023-01-23 18:10:38 -0500

    ACTUALLY fixed stuff.. im silly and stupid
    ykyk

--- 2023-Charged Up/src/main/java/frc/robot/subsystems/PnumaticSubsystem.java
+++ 2023-Charged Up/src/main/java/frc/robot/subsystems/PnumaticSubsystem.java
@@ -1,18 +1,20 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
+import frc.robot.oi;
 
 
 public class PnumaticSubsystem extends SubsystemBase {
-    public static Compressor compressor = new Compressor();
-    public static DoubleSolenoid solenoid = new DoubleSolenoid(0, 1);
+    public static Compressor compressor = new Compressor(null);
+    public static DoubleSolenoid solenoid = new DoubleSolenoid(null, 0, 1);
         // initializin' here
         static {
-            if (OI.getOperator().getBumper(Hand.kLeft)) { //gets input from the operator bumpers
+            if (oi.getOperator().getLeftBumper()) { //gets input from the operator bumpers
                 solenoid.set(DoubleSolenoid.Value.kForward); //changes voltage in the solenoid to make the hand close
             }
-            else if (OI.getOperator().getBumper(Hand.kRight)) { //may have to edit for right voltages teehee IDK
+            else if (oi.getOperator().getRightBumper()) { //may have to edit for right voltages teehee IDK
                 solenoid.set(DoubleSolenoid.Value.kReverse);
             }
 
