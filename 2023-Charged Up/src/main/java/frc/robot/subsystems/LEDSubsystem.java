1a31746264bea868bc2ce9b307917a060e9b8176
zirppo <zirppo@protonmail.com>
2023-01-27 22:32:23 -0500

    Update LEDSubsystem.java

--- 2023-Charged Up/src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ 2023-Charged Up/src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -1,60 +1,41 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.OI;
-
 import com.ctre.phoenix.led.CANdle;
 import com.ctre.phoenix.led.CANdleConfiguration;
 import com.ctre.phoenix.led.CANdle.LEDStripType;
 import edu.wpi.first.wpilibj.I2C;
-import edu.wpi.first.wpilibj.util.Color;
 
-public class LEDSubsystem extends SubsystemBase {
+public class LEDSubsystem extends SubsystemBase {  
+    static I2C.Port i2cPort;
+    static CANdle candle;
+    static CANdleConfiguration config;
+    static boolean toggle;
     
-    /* ex
-    CANdle candle;
-    I2C.Port i2cPort;   
-    */
-
-    // This block initializes all the static variables in the class
-    static {
-    // Declarations of variables should be moved outside any methods    
-    CANdle candle;
-    I2C.Port i2cPort;
-    }
-
-    public static void() { //set parameters for rgb and reorganize code
-        // All this code should be moved to the static block for initialization purposes
+    static { 
         CANdle candle = new CANdle(0);
         CANdleConfiguration config = new CANdleConfiguration();
         config.stripType = LEDStripType.RGB;
         candle.configAllSettings(config);
-        //
+    }
 
-        // This should be moved into the command part of the subsystem
-        if (oi.getOperator().getAButton()) { 
-            candle.setLEDs(166, 77, 255); 
-        }
-        else if (oi.getOperator().getBButton()) { 
-            candle.setLEDs(255, 195, 77);
+    public static void setLEDs(boolean aButtonPressed) {
+        if (aButtonPressed) {
+            toggle = true;
         }
 
-        // example to interact
-        /*
-            public static void setLEDs(boolean aButtonPressed, boolean bButtonPressed) {
-                if (aButtonPressed) { 
-                    candle.setLEDs(166, 77, 255); 
-                }
-                else if (bButtonPressed) { 
-                    candle.setLEDs(255, 195, 77);
-                }   
-            }
+        if (aButtonPressed && toggle == true) {
+            toggle = false;
+        }
 
-            in the command class, use the periodic method to get the a button and b button booleans and 
-            from the controller as parameters for the setLEDs method above. This will get the current state of 
-            the a and b buttons on the controller and set the LEDs accordingly
-        */
+        if (toggle == true) {
+            candle.setLEDs(166, 77, 255);
+        }
+
+        else {
+            candle.setLEDs(255, 230, 102);
+        }
+    }
 
     }
 
-}
