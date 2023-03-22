73d473ea069ad1246e5bfb59e6f55ef85359e5ad
JaxonAn23 <59897973+JaxonAn23@users.noreply.github.com>
2023-03-22 19:16:38 -0400

    Auton Finished, drive distance :-1:

--- 2023-Charged Up/src/main/java/frc/robot/commands/BlueRightAuton.java
+++ 2023-Charged Up/src/main/java/frc/robot/commands/BlueRightAuton.java
@@ -4,6 +4,7 @@
 
 package frc.robot.commands;
 import frc.robot.Robot;
+import frc.robot.subsystems.ClawWheelsSubsystem;
 import frc.robot.subsystems.LEDSubsystem;
 import frc.robot.subsystems.PistonClawSubsystem;
 import frc.robot.subsystems.PivotingArmSubsystem;
@@ -13,6 +14,7 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 
@@ -23,9 +25,12 @@ public class BlueRightAuton extends SequentialCommandGroup {
   private static TelescopingArmSubsystem m_TelescopingArmSubsystem;
   private static PistonClawSubsystem m_PistonClawSubsystem;
   private static LEDSubsystem m_LEDSubsystem;
+  private static ClawWheelAuton m_clawWheelAuton;
+  private static ClawWheelsSubsystem m_ClawWheelsSubsystem;
   static double[][] coordinates = {{0, 70.78, -255.11}, {0, 70.78, -172.61}};
   public BlueRightAuton(TelescopingArmSubsystem telescopingArmSubsystem, PivotingArmSubsystem pivotingArmSubsystem, 
-  SwerveDrivetrain swerveDrivetrain, PistonClawSubsystem pistonClawSubsystem, LEDSubsystem ledSubsystem) {
+  SwerveDrivetrain swerveDrivetrain, PistonClawSubsystem pistonClawSubsystem, LEDSubsystem ledSubsystem, 
+  ClawWheelAuton clawWheelAuton, ClawWheelsSubsystem clawWheelsSubsystem) {
 
     addRequirements(swerveDrivetrain, pivotingArmSubsystem, telescopingArmSubsystem, pistonClawSubsystem);
     m_TelescopingArmSubsystem = telescopingArmSubsystem;
@@ -33,18 +38,22 @@ public class BlueRightAuton extends SequentialCommandGroup {
     m_swerveDrivetrain = swerveDrivetrain;
     m_PistonClawSubsystem = pistonClawSubsystem;
     m_LEDSubsystem = ledSubsystem;
-
-    // addCommands(
-    //   new ArmAuton(m_PivotingArmSubsystem, m_TelescopingArmSubsystem, 2), //sets arm high
-    //   new ParallelCommandGroup(new ClawCommand(m_PistonClawSubsystem, m_LEDSubsystem), new WaitCommand(1)), //Dispenses cone
-    //   new ArmAuton(m_PivotingArmSubsystem, m_TelescopingArmSubsystem, 3), //sets arm to neutral position
-    //   new DriveDistance(m_swerveDrivetrain, new Pose2d(Units.inchesToMeters(121.61), Units.inchesToMeters(-255.11), new Rotation2d(0)), true, true), //Moves horizontally to allign with cone
-    //   new DriveDistance(m_swerveDrivetrain, new Pose2d(Units.inchesToMeters(121.61), Units.inchesToMeters(-64.62), new Rotation2d(0)), true, true), //Moves to central cone.
-    //   new ArmAuton(m_PivotingArmSubsystem, m_TelescopingArmSubsystem, 0), //Moves arm to pick up cone
-    //   //need wheel spinning here
-        
-
-    //   );
+    m_clawWheelAuton = clawWheelAuton;
+    m_ClawWheelsSubsystem = clawWheelsSubsystem;
+    addCommands(
+      new ArmAuton(m_PivotingArmSubsystem, m_TelescopingArmSubsystem, 2), //sets arm high
+      new ParallelCommandGroup(new ClawCommand(m_PistonClawSubsystem, m_LEDSubsystem), new WaitCommand(1)), //Dispenses cone
+      new ArmAuton(m_PivotingArmSubsystem, m_TelescopingArmSubsystem, 3), //sets arm to neutral position
+      new DriveDistance(m_swerveDrivetrain, new Pose2d(Units.inchesToMeters(121.61), Units.inchesToMeters(-255.11), new Rotation2d(0)), true, true), //Moves horizontally to allign with cone
+      new DriveDistance(m_swerveDrivetrain, new Pose2d(Units.inchesToMeters(121.61), Units.inchesToMeters(-64.62), new Rotation2d(180)), true, true), //Moves to central cone.
+      new ArmAuton(m_PivotingArmSubsystem, m_TelescopingArmSubsystem, 0), //Moves arm to pick up cone
+      new ParallelDeadlineGroup(new DriveDistance(m_swerveDrivetrain, new Pose2d(Units.inchesToMeters(121.61), Units.inchesToMeters(-60.62), new Rotation2d(180)), true, true), new ClawWheelAuton(0, null, isFinished()), new ClawWheelAuton(5, m_ClawWheelsSubsystem, true)),
+      new DriveDistance(m_swerveDrivetrain, new Pose2d(Units.inchesToMeters(121.61), Units.inchesToMeters(-255.11), new Rotation2d(0)), true, true), //Moves horizontally to allign with cone
+      new DriveDistance(m_swerveDrivetrain, new Pose2d(Units.inchesToMeters(93.61), Units.inchesToMeters(-255.11), new Rotation2d(0)), true, true), //Moves horizontally to allign with cone
+      new ArmAuton(m_PivotingArmSubsystem, m_TelescopingArmSubsystem, 2), //sets arm high
+      new ParallelCommandGroup(new ClawCommand(m_PistonClawSubsystem, m_LEDSubsystem), new WaitCommand(1)), //Dispenses cone
+      new ArmAuton(m_PivotingArmSubsystem, m_TelescopingArmSubsystem, 3) //sets arm to neutral position                                                                    
+      );
   }
 }
 
